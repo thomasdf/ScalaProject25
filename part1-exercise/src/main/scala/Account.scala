966f389b6275890bc14dbc5a9ad97b73f5209981
Thomas Frøysa <thomasdf@stud.ntnu.no>
2015-10-26 17:01:10 +0100

    Implement withdraw, deposit, getbalance
    Implemented with exceptions for illegal values

--- part1-exercise/src/main/scala/Account.scala
+++ part1-exercise/src/main/scala/Account.scala
@@ -1,10 +1,33 @@
 
-class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
-  
-  def withdraw(amount: Double): Unit = ??? //Implement
-  
-  def deposit(amount: Double): Unit = ??? //Implement
-  
-  def getBalanceAmount: Double = ??? //Implement
 
+import exceptions.IllegalAmountException
+import exceptions.NoSufficientFundsException
+
+class Account(initialBalance: Double, val uid: Int = Bank getUniqueId) {
+  /* 
+   * a class Account with a constructor which takes a variable double called initialBalance and a constant int called Uid,
+   * which it gets from the Bank
+   */
+  var balance:Double = initialBalance
+
+  def withdraw(amount: Double): Unit = { //Implement
+    if(amount < 0){
+      throw new IllegalAmountException
+    }else if(balance - amount < 0){
+      throw new NoSufficientFundsException
+    } else {
+      balance = balance - amount
+    }
+  }
+  def deposit(amount: Double): Unit = { //Implement
+    if(amount < 0){
+      throw new IllegalAmountException
+    } else {
+    	balance = balance + amount
+    }
+  }
+  
+  def getBalanceAmount: Double = { //Implement
+    balance
+  }
 }
