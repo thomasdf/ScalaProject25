7ecf9674c362769f2f0cfdf21eda285d2f155d7c
Thomas Frøysa <thomasdf@stud.ntnu.no>
2015-10-26 17:01:31 +0100

    simple implement: transaction

--- part1-exercise/src/main/scala/Bank.scala
+++ part1-exercise/src/main/scala/Bank.scala
@@ -4,8 +4,10 @@ object Bank {
   
   private var idCounter: Int = 0
   
-  def transaction(from: Account, to: Account, amount: Double): Unit = ??? //Implement
-  
+  def transaction(from: Account, to: Account, amount: Double): Unit = { //Implement
+    from.withdraw(amount)
+    to.deposit(amount)
+  }
   def getUniqueId: Int = {
     idCounter += 1
     idCounter
